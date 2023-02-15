fbd8ec425de1482bdbe382bc6f83ffe6a7a43ebf
Maksim Demakin <maxxdemakin@gmail.com>
2023-02-15 13:45:20 +0700

    Starting commit

---
+++ Семинары/lesson1/script.js
@@ -0,0 +1,36 @@
+// const c = 10;
+// const d = 2;
+// const summ = c + d;
+// console.log(summ);
+// const diff = c - d;
+// console.log(diff);
+// const composition = c * d;
+// console.log(composition);
+// const private = c / d;
+// console.log(private);
+// const summResults = summ + diff + composition + private;
+// console.log(summResults);
+
+// let a = 2 * (8 / 4);
+
+// console.log(a);
+
+// const a = 1.5;
+// const b = 0.75;
+// const summ = 1.5 + 0.75;
+// console.log(summ);
+
+// let a = 10;
+// a = - String(a);
+// a = Number(a);
+// console.log(a);
+
+// const firstName = 'Max'
+// const lastName = 'Demakin'
+// const stringResult = firstName + lastName;
+// console.log(stringResult);
+
+const firstWord = 'Hello';
+const secondWord = 'World';
+const stringResult = `${firstWord} ${secondWord}`;
+console.log(stringResult);
